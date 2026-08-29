63530a31a4e97f017fbac01a54630d905b3ea3f6
gift-dev575 <giftamelie@gmail.com>
2026-08-29 23:38:48 +0200

    mobile invasion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <>
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from '../../assets/assets'  
 
 const Footer = () => {
   return (
@@ -8,10 +9,32 @@ const Footer = () => {
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
             <p> We've got you covered. Browse top-rated restaurants, track your order in real time, and enjoy fresh food delivered straight to your doorstep.</p>
+            <div className="footer-social-icons"> 
+              <img src={assets.facebook_icon} alt= "FB icon" />
+              <img src= {assets.twitter_icon} alt=" X icon" />
+              <img src= {assets.linkedin_icon} alt="Linkedin icon" />
+            </div>
         </div>
-        <div className="footer-content-right"></div>
-        <div className="footer-content-center"></div>
+        
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+250-788-987-687</li>
+            <li>contact@chakuladel.com</li>
+          </ul>
+        </div>
       </div>
+      <hr />
+      <p className="footer-copyright"> Copyright 2026 © ChakulDel.com - All Right Reserved</p>
     </div>
   )
 }
