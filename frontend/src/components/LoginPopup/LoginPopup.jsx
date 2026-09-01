31d4e405f5476f3875fa74b4ac8e11c528910c67
gift-dev575 <giftamelie@gmail.com>
2026-09-01 20:27:56 +0200

    Structured cart into real

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import './LoginPopup.css' 
+import "./LoginPopup.css" 
 import { assets } from '../../assets/assets'
 
 const LoginPopup = ({setShowLogin}) => {
@@ -7,7 +7,7 @@ const LoginPopup = ({setShowLogin}) => {
     const [currState,setCurrState] = useState("Login")
   return (
     <div className='login-popup'>
-       <form action="" className="login-popup-container">
+       <form className="login-popup-container">
         <div className="login-popup-title">
             <h2>{currState}</h2>
             <img onClick = {()=>setShowLogin(false)}src={assets.cross_icon} alt="the Cross icon" />
@@ -20,8 +20,11 @@ const LoginPopup = ({setShowLogin}) => {
         <button>{currState === "Sign Up"?"Create account":"Login"}</button>
         <div className="login-popup-condition">
             <input type="checkbox" required/>
-            
+            <p>By continuing, I agree to terms of use & privacy policy</p>
         </div>
+        {currState==='Login'?<p>Create a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here</span></p>
+        :<p>Already have an account? <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+        }
     </form>
 </div>
   )
